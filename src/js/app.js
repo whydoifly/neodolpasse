f353b0d42e1b0ef7b918196edb34d49bbee71cec
whydoifly <60507914+whydoifly@users.noreply.github.com>
2022-08-15 15:24:17 +0300

    Добавил шрифты

--- src/js/app.js
+++ src/js/app.js
@@ -1,20 +1,18 @@
 import * as allFunctions from './modules/functions.js';
 // import swiperHey from './modules/swiper.js';
 
-
-
 allFunctions.isWebp();
 
 import Swiper, { Navigation, Pagination } from 'swiper';
 
-const swiperArticles = new Swiper(".articles-swiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    freeMode: true
+const swiperArticles = new Swiper('.articles-swiper', {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  freeMode: true,
 });
 
-const swiperResearches = new Swiper(".researches-swiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    freeMode: true
+const swiperResearches = new Swiper('.researches-swiper', {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  freeMode: true,
 });
